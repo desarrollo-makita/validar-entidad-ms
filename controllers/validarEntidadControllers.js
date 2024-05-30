3d7cfbd1d2958e94541d6c620e60786968d5d239
Jorge Herrera <jherrera@makita.cl>
2024-05-30 17:41:05 -0400

    ajustes de variables

--- controllers/validarEntidadControllers.js
+++ controllers/validarEntidadControllers.js
@@ -34,7 +34,7 @@ async function validarCliente(req , res){
         validacionCliente = result.recordset;
 
         if(!validacionCliente.length > 0) {
-            logger.info(`La entidad ${req.body.codigoPosto} no se encuentra en nuestros registros` 
+            logger.info(`La entidad ${req.body.entidad} no se encuentra en nuestros registros` 
             );
             return res.status(200).json({ mensaje: `La entidad ${req.body.entidad} no se encuentra en nuestros registros` });
         }
